6b75a473ba27fcde0c34d311b174b79f9ac10f25
Muhammadardan12 <149554891+Muhammadardan12@users.noreply.github.com>
2023-12-05 10:16:35 +0700

    Add files via upload

---
+++ GetStudentName.kt
@@ -0,0 +1,30 @@
+package arrays
+
+
+/**
+ * Parameter [listSiswa] berisi:
+ *
+ * [[Mamang, Sumanggimang, Mafdhil, Joko, Sujoko, Sumamang, Sangmamang, Siamang]]
+ *
+ * Coba dapatkan "Sujoko" melalui parameter [listSiswa]
+ *
+ */
+fun getStudentName(listSiswa: Array<String>): String {
+    var namaSiswa = ""
+
+    val listSiswa = arrayOf(
+        "Mamang",
+        "Sumanggimang",
+        "Mafadhil",
+        "Joko",
+        "Sujoko",
+        "Sumamang",
+        "Sangmamang",
+        "Siamang"
+    )
+
+    namaSiswa = listSiswa.get(3)
+
+
+    return namaSiswa
+}
